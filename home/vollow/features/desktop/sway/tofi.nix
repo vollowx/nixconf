fe4b74af66b19c993125201e107d7d9c0fbf91bd
Vollow <vollowx@proton.me>
2024-07-26 21:45:29 +0800

    feat(tofi): tweak appearance

--- home/vollow/features/desktop/sway/tofi.nix
+++ home/vollow/features/desktop/sway/tofi.nix
@@ -11,7 +11,7 @@
       width = "100%";
       height = 32;
       padding-left = 220;
-      padding-right = 460;
+      padding-right = 500;
       padding-top = 6;
       padding-bottom = 6;
       outline-width = 0;
@@ -19,16 +19,15 @@
       background-color = "#00000000";
       text-color = "#${colors.overlay0}";
 
-      prompt-text = "(";
+      prompt-text = "\"(run \"";
       prompt-background-padding = "0, 15";
       prompt-color = "#${colors.mauve}";
 
       input-color = "#${colors.teal}";
-      input-padding = "0, 15";
 
       result-spacing = 15;
-      selection-color = "#${colors.subtext1}";
-      selection-match-color = "#${colors.subtext0}";
+      selection-color = "#${colors.text}";
+      selection-match-color = "#${colors.text}";
     };
   };
 }
