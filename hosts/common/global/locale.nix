73d96430a72a36a0cf703a5644f700fcfb034816
Vollow <vollowx@proton.me>
2024-07-28 19:07:00 +0800

    feat(locale): use `en_US`

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -3,7 +3,7 @@
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_TIME = lib.mkDefault "zh_CN.UTF-8";
+      LC_TIME = lib.mkDefault "en_US.UTF-8";
     };
     supportedLocales = lib.mkDefault [
       "en_US.UTF-8/UTF-8"
